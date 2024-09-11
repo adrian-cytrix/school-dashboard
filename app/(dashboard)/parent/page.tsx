93af715552a8f59a0b7b16ad656970312ed1b309
Adrian <cytrix.adrian@gmail.com>
2024-09-12 07:09:38 +0800

    add image on dashboard layout

---
+++ app/(dashboard)/parent/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ParentView() {
+  return (
+    <div>ParentView</div>
+  )
+}
---
+++ app/(dashboard)/teacher/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TeacherView() {
+  return (
+    <div>TeacherView</div>
+  )
+}
